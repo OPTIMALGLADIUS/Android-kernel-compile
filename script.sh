43d28b79e46cb8df14ee778785f76511b7caed6c
OPTIMALGLADIUS <87300828+OPTIMALGLADIUS@users.noreply.github.com>
2024-06-13 16:42:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,12 +11,12 @@ sudo apt remove openssl -y
 sudo apt remove libssl-dev -y
 
 #Some openssl dependencies
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl-dev_1.1.1f-1ubuntu2.22_amd64.deb
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/openssl_1.1.1f-1ubuntu2.22_amd64.deb
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl-dev_1.1.1f-1ubuntu2.22_amd64.deb -O libssl-dev
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/openssl_1.1.1f-1ubuntu2.22_amd64.deb -O openssl
 
 #Installing
-sudo dpkg --install ‘openssl_1.1.1f-1ubuntu2.22_amd64.deb’ -y
-sudo dpkg --install ‘libssl-dev_1.1.1f-1ubuntu2.22_amd64.deb’ -y
+sudo dpkg --install openssl -y
+sudo dpkg --install libssl-dev -y
 
 #This is to install necessary packages
 sudo apt install git ccache automake cpio flex lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx-doc libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven pwgen libswitch-perl policycoreutils minicom libxml-sax-base-perl libxml-simple-perl bc libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev xsltproc unzip -y
