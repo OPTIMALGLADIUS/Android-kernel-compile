6e000a7b0c2debfec5edf79278272cc47377ccbb
OPTIMALGLADIUS <87300828+OPTIMALGLADIUS@users.noreply.github.com>
2025-09-02 11:04:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 git clone --depth=1 https://github.com/OPTIMALGLADIUS/android_kernel_6.1_nothing_mt6878.git -b mt6878/Tetris/v cmf1
 
 #Building kernel with full LTO
-tools/bazel build --config=fast //common:kernel_aarch64_dist
+tools/bazel build //common:kernel_aarch64_dist
 
 #Packing the out directory
 zip -r kernel.zip /home/runner/work/Android-kernel-compile/Android-kernel-compile/build/out
