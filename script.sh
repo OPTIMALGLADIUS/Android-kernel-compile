3ed03159e9de06d94c36f1e5f3c4e197ff48e96c
OPTIMALGLADIUS <87300828+OPTIMALGLADIUS@users.noreply.github.com>
2025-09-05 20:00:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Synchronize Google's kernel toolchains
+repo --trace sync -c -j$(nproc --all) --no-tags --fail-fast
+
 #Getting the kernel source
 git clone --depth=1 https://github.com/OPTIMALGLADIUS/android_kernel_6.1_nothing_mt6878.git -b mt6878/Tetris/v cmf1
 
